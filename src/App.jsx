0d7ce423ea514d17daf2dcc4ea09b6afc749220c
GustavoFFelipe <gustavo.ffelipedev@gmail.com>
2022-07-09 20:12:30 -0300

    aula 59

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm'
+import './App.css';
+import Users from './components/Users';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/*<AddUserForm />*/}
+      {/*<EditUserForm />*/}
+      <Users />
+    </div>
+  );
+}
+
+export default App;
